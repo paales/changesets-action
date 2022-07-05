aefbbdca5791056578004d453d0c1ee3b83c6aa1
Dotan Simha <dotansimha@gmail.com>
2022-07-05 18:17:50 +0300

    fix for supporting "false" in config

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,8 @@ function extractCreateGithubReleases(
     return "aggregate";
   } else if (input === "true") {
     return true;
+  } else if (input === "false") {
+    return false;
   }
 
   core.warning(
